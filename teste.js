338d60276d908d89038a8c358efdb31d74e1969b
Wesley Santana <39658223+WesleyRSan@users.noreply.github.com>
2018-10-27 11:29:37 -0300

    Detalhe Cep's e Msg (1) Erro
    Detalhamento dos Cep's encontrados e Msg de Erro apenas para Formatos errados de Cep informados pelo usuário.

--- teste.js
+++ teste.js
@@ -5,60 +5,43 @@ function iniciar(cepInformado) {
     console.log(url)
     $.ajax({
         url,
-        dataType: 'json',
+        dataType: "json",
         success(retornoDaApi){
             dados = retornoDaApi;
             console.log(dados);
             endereco(dados);
         },
-        error(e){            
+        error(e){
+            if (e.erro = true) {
+                alert("Formato de CEP invalido")
+            }
+            //  else if (e.erro = ) {                alert("CEP não localizado")                           }
         },
     })
 }
 
-$('#consultaCep').click(function(){
-    var cepIndicado = $('#numeroCep').val();
+$("#consultaCep").click(function(){
+    var cepIndicado = $("#numeroCep").val();
     iniciar(cepIndicado);
 });
 
-// o codigo abaixo é um exemplo de outro projeto
-    // $("#listagem").on('click', '.excluir', function(event) {
-    //     const index = $(this).attr('href');
-    //     $(`#pokemon-${index}`).remove();
-    //     event.preventDefault();
-    // });
-
-
-// o bloco abaixo deu certo como link do titulo informado no html "Encontre seu CEP". Como é para teste solicitei so um console e um alert
-$('.testelink').on('click', function(event){
-    console.log('Esse é o cep clicado')
-    event.preventDefault();
-    alert("Was preventDefault() called: " + event.isDefaultPrevented());
-});
-
-
 // o codigo abaixo deu errado, é um button gerado junto com as opções de cep's recebidas pelo endereço indicado e como é só teste pedi só um console e um alert (trabalhei pouco nele)
-$('.escolheCep').on('click', function(event){
-    console.log('Esse é o cep clicado')
+$(".escolheCep").on("click", function(event){
+    console.log("Esse é o cep clicado")
     event.preventDefault();
     alert("Was preventDefault() called: " + event.isDefaultPrevented());
 });
 
-// o bloco abaixo deu errado, é para clicar nos links dos cep's gerado apos informar os dados do endereço em busca do CEP e apresenta-lo detalhado na terceira coluna
-$('.detalhaCep').on('click', function(event){
-    var cepClicado = $(this).attr('href');
-    console.log('Esse é o cep clicado'+ cepClicado)
+// Clicar nos links dos cep's gerados e apresenta-lo detalhado na terceira coluna
+$("#cepEncontrado").on("click", ".detalhaCep", function(event){
+    var cepClicado = $(this).attr("href");
+    console.log("Esse é o cep clicado"+ cepClicado)
     iniciar(cepClicado);
     event.preventDefault();
 });
 
-// AS tres opções abaixo surgiram nas pesquisas como possíveis substitutas do preventDefault, mas nao analisei nenhuma
-    // return false()
-    // event.stopPropagation();
-    // disable;
-
 const endereco = function(){
-    $('#endereco').html(`
+    $("#endereco").html(`
         <table class="table table-hover">
             <thead>
                 <tr>
@@ -96,13 +79,13 @@ const endereco = function(){
 
 const acharCep = function(){
 
-    const estado = $('#estado').val();
-    const cidade = $('#cidade').val();
-    const logradouro = $('#logradouro').val();
+    const estado = $("#estado").val();
+    const cidade = $("#cidade").val();
+    const logradouro = $("#logradouro").val();
 
     $.ajax({
         url: `https://viacep.com.br/ws/${estado}/${cidade}/${logradouro}/json/`,
-        dataType: 'json',
+        dataType: "json",
         success(retorno) {
             dados = retorno;
             opcoesDeCep(dados)
@@ -113,7 +96,7 @@ const acharCep = function(){
     })
 }
 
-$('#procuraCep').click(function(){
+$("#procuraCep").click(function(){
     acharCep();
 });
 
@@ -127,11 +110,11 @@ const opcoesDeCep = function() {
                     <td><a href="${item.cep}" class="detalhaCep">${item.cep}</a></td>
                     <td>${item.logradouro}</td>
                     <td>${item.bairro}</td>
-                    <td><button class='escolheCep'> Consultar </button></td>
+                    <td><button class="escolheCep"> Consultar </button></td>
                 </tr>                    
             </table>`
         
-        $('#CepEncontrado').html(html);
+        $("#cepEncontrado").html(html);
     })
 }
 
